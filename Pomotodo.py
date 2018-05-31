1eb1e3f56e5bb5ab96045e2b79e23f6391368060
Thomas Liu <liuxk99@gmail.com>
2018-05-31 11:29:42 +0800

    <feature>: dump pomotodo activity for seperated task.

--- Pomotodo.py
+++ Pomotodo.py
@@ -1,13 +1,44 @@
 # coding=utf-8
+import datetime
 import sys
 
-pomo = u"09:45 - 10:10 #人文/语言/英语 '单词·知米背单词' |2018.05.30 + #财经/理财/投资/股票 '乐视网·操盘' |2018.05.30"
-split = u" + "
+act1 = u"09:49 - 10:14\n#时间管理 '日·计划' |2018.05.31 + #生活/日常 '天气' |2018.05.31 + #人文/语言/英语 '单词·知米背单词' |2018.05.31"
+
+taskSep = u" + "
+
+def parse_pomo_activity(activity):
+    lines = activity.splitlines()
+    # for line in lines:
+    #     print line
+
+    actTime = datetime.datetime.now().strftime("%Y.%m.%d") + u" " + lines[0]
+    # print "time: " + actTime
+
+    actTasks = lines[1].split(taskSep)
+    pos = actTasks[0].find(u"#")
+    if (pos == 0):
+        actTasks[0] = actTasks[0].replace(u"#", u"\#")
+    for task in actTasks:
+        # print "task: " + task
+        pass
+
+    print actTime
+    print taskSep.join(actTasks)
+
+    for i in range(len(actTasks)):
+        dumpTasks = actTasks[:]
+
+        print
+        print actTime
+        task = u"**" + actTasks[i] + u"**"
+        dumpTasks[i] = task
+        print taskSep.join(dumpTasks)
+
+    pass
 
 def main(argv):
-    tasks = pomo.split(split)
-    for task in tasks:
-        print task
+    print u"\#"
+    parse_pomo_activity(act1)
     pass
 
 if __name__ == '__main__':
---
+++ test_pomotodo.py
@@ -0,0 +1,14 @@
+# coding=utf-8
+from unittest import TestCase
+
+from Pomotodo import parse_pomo_activity
+
+
+class TestParse_pomo_activity(TestCase):
+    def test_parse_pomo_activity(self):
+        act1 = u"09:49 - 10:14\n#时间管理 '日·计划' |2018.05.31 + #生活/日常 '天气' |2018.05.31 + #人文/语言/英语 '单词·知米背单词' |2018.05.31"
+        parse_pomo_activity(act1)
+
+        act2 = u"11:12 - 11:43\n#时间管理/工具 'pomotodo·parser'"
+        parse_pomo_activity(act2)
+        pass

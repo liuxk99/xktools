3770a0a5bfac2573a4b668bff59772c6c9b02a44
Thomas Liu <liuxk99@gmail.com>
2019-12-18 10:02:58 +0800

    <Refactoring>: extract method.

--- test_pomos_csv_parser.py
+++ test_pomos_csv_parser.py
@@ -4,22 +4,30 @@ from unittest import TestCase
 import pomos_csv_parser
 
 
+def parse_csv_file(download_path, csv_file):
+    in_file = download_path + os.sep + csv_file
+    if os.path.exists(in_file):
+        pomos_csv_parser.parse_pomos_csv_file(in_file, "trello.md")
+    else:
+        print "'%s' is not exist!" % in_file
+    pass
+
+
 class Test(TestCase):
     def test_parse_pomos_csv_file_unix(self):
-        download_path="/home/thomas/Downloads"
+        download_path = "/home/thomas/Downloads"
         csv_file = "Pomos - 2019-12-16 - 2019-12-16.csv"
-        pomos_csv_parser.parse_pomos_csv_file(download_path + os.sep + csv_file, "trello.md")
+
+        parse_csv_file(download_path, csv_file)
         # self.fail()
+
     pass
 
     def test_parse_pomos_csv_file_win(self):
-        download_path=r"C:\Users\thomas\Downloads"
+        download_path = r"C:\Users\thomas\Downloads"
         csv_file = r"Pomos - 2019-12-16 - 2019-12-16.csv"
 
-        in_file = download_path + os.sep + csv_file
-        if os.path.exists(in_file):
-            pomos_csv_parser.parse_pomos_csv_file(in_file, "trello.md")
-        else:
-            print "'%s' is not exist!" % in_file
+        parse_csv_file(download_path, csv_file)
         # self.fail()
+
     pass

c2245c7470e0be08aea479877743388ffe129190
Thomas Liu <liuxk99@gmail.com>
2019-12-30 18:22:07 +0800

    <feature>: trello dictionary.

--- trello.py
+++ trello.py
@@ -10,7 +10,17 @@ trello_dict = {
     "#生活/行/车(比亚迪·秦) '充电·": "https://trello.com/c/JotLTWjl",
     "#生命/健康 '锻炼·散步'": "https://trello.com/c/T3DBFGe8",
     "#ADE(Android)/DTC/BS 'Gradle·plugin'": "https://trello.com/c/MCzn1NMi",
+    "#ADE(Android)/DTC/BS 'Gradle·Sync": "https://trello.com/c/tlNQ7ZwL",
     "#时间管理 '休假·": "https://trello.com/c/IwnTjw5z",
     "#社交/亲/子(刘枢鸿) '作业·": "https://trello.com/c/OP2j3pkV",
-    "#生活/食 '做饭": "https://trello.com/c/n33fHx8L"
+    "#生活/食 '做饭": "https://trello.com/c/n33fHx8L",
+    "#时间管理 '工具": "https://trello.com/c/5uLm7R7g",
+    "#产品(EUI)/App '全视频TV桌面": "https://trello.com/c/J4f7LxXs",
+    "#项目管理 '方法(康桦)": "https://trello.com/c/WZbJWhQh",
+    "#软件/应用 '笔记(有道云笔记)": "https://trello.com/c/ZvvtYvkc",
+    # 月度
+    "#金融/服务/银行(中行) '信用卡": "https://trello.com/c/jfOdGClU",
+    "#金融/服务/银行(招行) '信用卡": "https://trello.com/c/fHAuY8ht",
+    "#社交/友 '施王兴": "https://trello.com/c/7Zr1ghMA",
+    "#生活/行/车(福特·蒙迪欧) '进京证": "https://trello.com/c/p1JrhG0Y",
 }

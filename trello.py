ae9c428499948a6eef1a545726c339cf841812c3
Thomas Liu <liuxk99@gmail.com>
2020-03-10 20:01:38 +0800

    [Dev]<trello/dictionary>: update(2020/03/10).

--- trello.py
+++ trello.py
@@ -136,4 +136,5 @@ trello_dict = {
     "#Product(Android)/Component 'PRODUCT_BOOT_JARS·": "https://trello.com/c/E2bRfSuH",
     "#产品(EUI)/特性/性能 '内存·": "https://trello.com/c/CKePjKrH",
     "#产品(EUI)/规划 '实例(CVTE)·": "https://trello.com/c/CKePjKrH",
+    "#产品(EUI) 'Arch·": "https://trello.com/c/x0izUJVq",
 }
